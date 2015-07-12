d0d95c7427718771cba39c8a3422aeb3dff37f15
Oleg Vaskevich <oleg@osv.im>
2015-07-12 08:41:01 -0700

    Add swipe to delete

--- AbusePrevention/app/src/main/java/chimehack/abuseprevention/ui/MainActivity.java
+++ AbusePrevention/app/src/main/java/chimehack/abuseprevention/ui/MainActivity.java
@@ -2,44 +2,54 @@ package chimehack.abuseprevention.ui;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
-
-import java.io.IOException;
-import java.util.List;
-import android.content.Context;
-import android.view.ViewGroup;
-import android.view.View;
 import android.widget.TextView;
-import java.util.ArrayList;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.util.Scanner;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import chimehack.abuseprevention.R;
 
 public class MainActivity extends Activity {
 
-    ListView mTodoList;
-    TodoAdapter todoAdapter;
+    private SwipeMenuListView mTodoList;
+    private TodoAdapter todoAdapter;
+    private List<JSONObject> mTodos;
+    private static int[] COLORS = {0xFF1CB17B, 0xFF3A85AB, 0xFFF26960, 0xFF93CBF0, 0xFF59C4AC,
+            0xFFF0AF44};
 
     private static class TodoAdapter extends ArrayAdapter<JSONObject> {
 
         final Context mContext;
-        final List<JSONObject> mTodos;
+        List<JSONObject> mTodos;
 
         TodoAdapter(Context context, List<JSONObject> todos) {
             super(context, R.layout.task_view, todos);
@@ -54,12 +64,19 @@ public class MainActivity extends Activity {
                 convertView = inflater.inflate(R.layout.task_view, parent, false);
             }
 
-            TextView task = (TextView)convertView.findViewById(R.id.taskTextView);
+            TextView task = (TextView) convertView.findViewById(R.id.taskTextView);
+            View border = convertView.findViewById(R.id.rightHandBorder);
 
             JSONObject todo = mTodos.get(position);
             task.setText(todo.optString("task", "Unknown"));
+            border.setBackgroundColor(COLORS[position % COLORS.length]);
 
             return convertView;
+        }
+
+        public void update(List<JSONObject> todos) {
+            mTodos = todos;
+            notifyDataSetChanged();
         }
     }
 
@@ -68,42 +85,82 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTodoList = (ListView) findViewById(R.id.todoList);
+        mTodoList = (SwipeMenuListView) findViewById(R.id.todoList);
 
+        mTodos = new ArrayList<JSONObject>();
         try {
             String content = new Scanner(new File(this.getFilesDir(), "todos.json")).useDelimiter("\\Z").next();
             try {
                 JSONArray tasks = new JSONArray(new JSONTokener(content));
-                ArrayList<JSONObject> todos = new ArrayList<JSONObject>();
-                for(int i = 0; i < tasks.length(); i++){
-                    todos.add(tasks.getJSONObject(i));
+                for (int i = 0; i < tasks.length(); i++) {
+                    mTodos.add(tasks.getJSONObject(i));
                 }
-                todoAdapter = new TodoAdapter(this, todos);
                 Log.d("MainActivity", "Loaded tasks");
             } catch (org.json.JSONException e) {
                 Log.e("MainActivity", "Error loading todo file");
-                todoAdapter = new TodoAdapter(this, new ArrayList<JSONObject>());
             }
         } catch (IOException e) {
             // react
             Log.d("MainActivity", "Failed to load tasks, it didn't exist");
-            todoAdapter = new TodoAdapter(this, new ArrayList<JSONObject>());
             String[] prePopulate = new String[]{
                     "Pick up groceries",
                     "Check bank account balance",
                     "Check the mail"
             };
-            for(String todo : prePopulate) {
+            for (String todo : prePopulate) {
                 JSONObject newTask = new JSONObject();
                 try {
                     newTask.put("task", todo);
                 } catch (org.json.JSONException ex) {
                     Log.e("MainActivity", "Error prepopulating tasks");
                 }
-                todoAdapter.add(newTask);
+                mTodos.add(newTask);
             }
         }
+        todoAdapter = new TodoAdapter(this, mTodos);
+//        mTodoList.setOnSwipeListener(new SwipeMenuListView.OnSwipeListener() {
+//            @Override
+//            public void onSwipeStart(int i) {
+//                // Nothing.
+//            }
+//
+//            @Override
+//            public void onSwipeEnd(int i) {
+//                mTodos.remove(i);
+//                todoAdapter.update(mTodos);
+//                saveToFile(mTodos.toString());
+//            }
+//        });
+//        mTodoList.setOpenInterpolator(new LinearInterpolator(this, null));
+        SwipeMenuCreator swipeToDelete = new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu swipeMenu) {
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 90,
+                        getResources().getDisplayMetrics()));
+                // set a icon
+                deleteItem.setIcon(android.R.drawable.ic_delete);
+                // add to menu
+                swipeMenu.addMenuItem(deleteItem);
+            }
+        };
         mTodoList.setAdapter(todoAdapter);
+        mTodoList.setMenuCreator(swipeToDelete);
+        mTodoList.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int i, SwipeMenu swipeMenu, int i1) {
+                mTodos.remove(i);
+                todoAdapter.update(mTodos);
+                saveToFile(mTodos.toString());
+                return true;
+            }
+        });
     }
 
     @Override
@@ -169,29 +226,6 @@ public class MainActivity extends Activity {
             Log.d("MainActivity", "Wrote out to file");
         } catch (IOException e) {
             Log.e("MainActivity", "Unable to write out todos save file");
-        }
-    }
-
-    public void onDoneButtonClick(View view) {
-        View v = (View)view.getParent();
-        TextView taskTextView = (TextView)v.findViewById(R.id.taskTextView);
-        String task = taskTextView.getText().toString();
-        for(int i = 0; i < todoAdapter.mTodos.size(); i++){
-            JSONObject storedTask = todoAdapter.mTodos.get(i);
-            String storedTodo = "";
-            try {
-                storedTodo = (String)storedTask.get("task");
-            } catch (org.json.JSONException e) {
-                // React
-                return;
-            }
-            if (storedTodo.equals(task)) {
-                todoAdapter.remove(storedTask);
-                // Save current list of tasks
-                String currTasks = todoAdapter.mTodos.toString();
-                saveToFile(currTasks);
-                return;
-            }
         }
     }
 }
