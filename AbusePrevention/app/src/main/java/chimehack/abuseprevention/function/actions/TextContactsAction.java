82b0904b2183f0158a45a8a1b39956ac6540a117
Linda Zheng <lzheng@shapesecurity.com>
2015-07-12 08:59:54 -0700

    finished message sending

--- AbusePrevention/app/src/main/java/chimehack/abuseprevention/function/actions/TextContactsAction.java
+++ AbusePrevention/app/src/main/java/chimehack/abuseprevention/function/actions/TextContactsAction.java
@@ -37,7 +37,20 @@ public class TextContactsAction implements Action {
                     appName, fullName, homeAddress, currentLocation, canCall, canText, firstName,
                     additionalMessage);
             Log.i(Constants.TAG, "Sending text message: " + message);
+            smsManager.sendTextMessage(contact.phoneNumber, null, message, null, null);
 //            smsManager.sendTextMessage(); // TODO (linda)
+//=======
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append("This is an automated report from [our app], sent on behalf of Linda Zheng.\n");
+//        stringBuilder.append("Home Address: \n");
+//        stringBuilder.append("Current estimated coordinates: \n");
+//
+//        for (Config.EmergencyContact contact : service.getConfig().emergencyContacts) {
+//            stringBuilder.append("Please " + (contact.canCall ? "feel free to " : "DO NOT ") + "call me.\n");
+//            stringBuilder.append("Please " + (contact.canText ? "feel free to " : "DO NOT ") + "text me.\n");
+//            stringBuilder.append("Additional notes from Linda: " + customMessages.get(contact.name));
+//            smsManager.sendTextMessage(contact.phoneNumber, null, stringBuilder.toString(), null, null); // TODO (linda)
+//>>>>>>> finished message sending
 
         }
     }
