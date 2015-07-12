05741951005b4aef2638efaabe117273b3e5981f
Oleg Vaskevich <oleg@osv.im>
2015-07-12 12:51:50 -0700

    Remove duplicate method

--- AbusePrevention/app/src/main/java/chimehack/abuseprevention/function/Config.java
+++ AbusePrevention/app/src/main/java/chimehack/abuseprevention/function/Config.java
@@ -153,10 +153,6 @@ public class Config {
         emergencyContacts.add(contact);
     }
 
-    public void addStatement(Statement statement) {
-        statements.add(statement);
-    }
-
     public void removeEmergencyContact(int contactId) {
         emergencyContacts.remove(contactId);
     }
