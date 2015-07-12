827ef08c28b9f0d949c0a98edaae772e7fa756ca
Linda Zheng <lzheng@shapesecurity.com>
2015-07-11 17:08:01 -0700

    settings

--- AbusePrevention/app/src/main/java/chimehack/abuseprevention/ui/AdvancedPrefsActivity.java
+++ AbusePrevention/app/src/main/java/chimehack/abuseprevention/ui/AdvancedPrefsActivity.java
@@ -1,6 +1,22 @@
 package chimehack.abuseprevention.ui;
 
-import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class AdvancedPrefsActivity extends Activity {
+import chimehack.abuseprevention.R;
+
+public class AdvancedPrefsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.prefs);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    }
 }
--- AbusePrevention/app/src/main/java/chimehack/abuseprevention/ui/PrefsActivity.java
+++ AbusePrevention/app/src/main/java/chimehack/abuseprevention/ui/PrefsActivity.java
@@ -1,14 +1,40 @@
 package chimehack.abuseprevention.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import chimehack.abuseprevention.R;
 
-public class PrefsActivity extends PreferenceActivity {
+public class PrefsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("pref_advanced")) {
+            startActivity(new Intent(this, AdvancedPrefsActivity.class));
+        } else if (key.equals("pref_checkbox")) {
+            Preference connectionPref = findPreference(key);
+            connectionPref.setSummary(sharedPreferences.getString(key, ""));
+            Log.d("PrefsActivity", sharedPreferences.getString(key, ""));
+        } else if (key.equals("pref_font")) {
+            Preference connectionPref = findPreference(key);
+            connectionPref.setSummary(sharedPreferences.getString(key, ""));
+        } else if (key.equals("pref_theme")) {
+            Preference connectionPref = findPreference(key);
+            connectionPref.setSummary(sharedPreferences.getString(key, ""));
+        } else if (key.equals("pref_max_items")) {
+            Preference connectionPref = findPreference(key);
+            connectionPref.setSummary(sharedPreferences.getString(key, ""));
+        }
     }
 }
